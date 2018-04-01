82287091210693670d0467de976145dd8b12a5ee
Himanshu Paul <hpaul2094@gmail.com>
2018-04-01 17:22:13 -0400

    Added damage to telek obj and Flame off on freeze

---
+++ Elemental Rift/Assets/Scripts/FlameTorch.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameTorch : MonoBehaviour {
+
+    public GameObject flamelight;
+    public GameObject flame;
+
+    bool isActive;
+	// Use this for initialization
+	void Start () {
+		if(flame.activeInHierarchy && flamelight.activeInHierarchy)
+        {
+            isActive = true;
+        }
+        else
+        {
+            isActive = false;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "FreezeProjectile" && isActive == true)
+        {
+
+            flamelight.SetActive(false);
+            flame.SetActive(false);
+            isActive = false;
+        }
+            
+
+    }
+}
---
+++ Elemental Rift/Assets/Scripts/TelekDmg.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TelekDmg : MonoBehaviour {
+
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "RockEnemy")
+        {
+            collision.gameObject.GetComponent<EnemyHealth>().TakeDamage(10);
+            Destroy(gameObject);
+        }
+    }
+}
