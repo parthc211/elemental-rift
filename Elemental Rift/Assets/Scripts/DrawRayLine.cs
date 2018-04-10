b8ab45cafefb79973550ea5b26f7073f4fd725c2
Himanshu Paul <hpaul2094@gmail.com>
2018-04-09 21:26:43 -0400

    Sync

--- Elemental Rift/Assets/Scripts/DrawRayLine.cs
+++ Elemental Rift/Assets/Scripts/DrawRayLine.cs
@@ -25,14 +25,17 @@ public class DrawRayLine : MonoBehaviour
     {
         dir = target.transform.position - transform.position;
         Physics.Raycast(transform.position, dir, out rayInfo);
-        
-        incomingVec = rayInfo.point - transform.position;
-        reflectVec = Vector3.Reflect(incomingVec, rayInfo.normal);
-        reflectVec *= 5;
-        Physics.Raycast(rayInfo.point, reflectVec, out reflectInfo);
+        if(rayInfo.collider.tag == "IceShield")
+        {
+            incomingVec = rayInfo.point - transform.position;
+            reflectVec = Vector3.Reflect(incomingVec, rayInfo.normal);
+            //reflectVec *= 5;
+            Physics.Raycast(rayInfo.point, reflectVec, out reflectInfo);
+            Debug.DrawRay(rayInfo.point, reflectVec, Color.blue);
+        }
         
         
         Debug.DrawRay(transform.position, rayInfo.point - transform.position, Color.red);
-        Debug.DrawRay(rayInfo.point, reflectInfo.point - rayInfo.point, Color.blue);
+        //Debug.DrawRay(rayInfo.point, reflectVec, Color.blue);
     }
 }
