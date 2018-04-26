29a06aa99c052b17cb3bcad53697635bc1c00d95
ruchitak3030 <rdk8868@g.rit.edu>
2018-04-26 13:54:12 -0400

    Thunder Enemy health bar system added

--- Elemental Rift/Assets/Scripts/ThunderEnemyScripts/ThunderEnemyHealth.cs
+++ Elemental Rift/Assets/Scripts/ThunderEnemyScripts/ThunderEnemyHealth.cs
@@ -1,21 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ThunderEnemyHealth : MonoBehaviour {
 
     // Use this for initialization
     public float thunderHealth = 100f;
 
+    public Slider healthBar;
+
+    private GameObject player;
+
 	void Start ()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
+        healthBar.maxValue = thunderHealth;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
+        healthBar.value = thunderHealth;
 
+        Vector3 target = player.transform.position - gameObject.transform.position;
+        Quaternion lookAtPlayer = Quaternion.LookRotation(target);
+        healthBar.transform.rotation = lookAtPlayer;
 	}
 
     public void TakeDamage(float damageAmount)
