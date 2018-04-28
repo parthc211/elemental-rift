18aebeae0498f4083cf2e5c82ffa257ea7c23180
Himanshu Paul <hpaul2094@gmail.com>
2018-04-28 02:13:48 -0400

    Cutscene added

---
+++ Elemental Rift/Assets/Scripts/cutscenetransition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class cutscenetransition : MonoBehaviour {
+    float timer;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+
+        if(timer >= 31.0f)
+        {
+            SceneManager.LoadScene("Prototype_Earth_Level");
+        }
+	}
+}
---
+++ Elemental Rift/Assets/Scripts/onbreakcrystal.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class onbreakcrystal : MonoBehaviour {
+
+    float timer;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if(timer > 3.0f)
+        {
+            SceneManager.LoadScene("Cutscene");
+        }
+
+    }
+}
