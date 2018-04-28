b66d8f04eb4d32e2db9a3ef95d3a138c86d1740a
ruchitak3030 <rdk8868@g.rit.edu>
2018-04-28 01:03:10 -0400

    Created Main Screen UI

---
+++ Elemental Rift/Assets/Scripts/GameManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject playButton;
+    public GameObject instructionsButton;
+    public GameObject creditsButton;
+    public GameObject exitButton;
+
+    public GameObject mainScreen;
+    public GameObject instructionsScreen;
+    public GameObject creditsScreen;
+
+    private void Awake()
+    {
+        mainScreen.SetActive(true);
+        instructionsScreen.SetActive(false);
+        creditsScreen.SetActive(false);
+    }
+    void Start ()
+    {
+		
+	}
+	
+
+	void Update ()
+    {
+		
+	}
+
+    public void OnClickPlayButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnClickInstructionButton()
+    {
+        mainScreen.SetActive(false);
+        instructionsScreen.SetActive(true);
+        creditsScreen.SetActive(false);
+    }
+
+    public void OnClickCreditsButton()
+    {
+        mainScreen.SetActive(false);
+        instructionsScreen.SetActive(false);
+        creditsScreen.SetActive(true);
+    }
+
+    public void OnClickExitButton()
+    {
+        Application.Quit();
+    }
+
+    public void OnClickBackButton()
+    {
+        mainScreen.SetActive(true);
+        instructionsScreen.SetActive(false);
+        creditsScreen.SetActive(false);
+    }
+}
